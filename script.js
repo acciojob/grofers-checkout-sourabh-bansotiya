4a388ec9eea1cb42a8c5fd38b0f9e39c90f8fa74
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-08 13:41:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+
+const totalPriceElement = document.createElement("div");
+totalPriceElement.id = "ans";
+totalPriceElement.textContent = ""; 
+
+
+document.body.appendChild(totalPriceElement);
+
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.textContent = "Get Total Price";
+
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	let sum = 0;
-  const rupee = document.querySelectorAll('.price');
+    let sum = 0;
+    const prices = document.querySelectorAll('.price');
+    
+    prices.forEach(price => {
+        sum += parseInt(price.textContent);
+    });
 
-	rupee.forEach(price => {
-		sum += parseInt(price.textContent);
-	});
-	const table = document.querySelector('table');
-	const newRow = table.insertRow(-1);
-	const cell = newRow.insertCell(0);
-	cell.textContent = "Total Price : Rs" + sum;
+    totalPriceElement.textContent = "Total Price: Rs " + sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
